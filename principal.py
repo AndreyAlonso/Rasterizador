c034cdf0172ee645c52a197c6de41d4a0679bf01
Andrey Hernandez <hec_boy@hotmail.com>
2019-11-07 02:58:40 -0600

    Se empieza a realizar rasterizacion (MEJORAR)

--- principal.py
+++ principal.py
@@ -7,6 +7,65 @@
 #############################################################
 
 import sys, pygame
+
+
+def obtenTam():
+    n = int(input("Tam de la matriz: "))
+    return n
+
+def generaCuadricula(N, x1,y1,x2,y2,screen, cuadro):
+    distanciaX = int((x2 - x1)/N)
+    distanciaY = int((y2 - y1)/N)
+    i = x1
+    j = y1
+
+    while i <= x2 or j <= y2:
+        color = (40,210,250)
+        starPosX = (i,y1)
+        endPosX  = (i,y2)
+
+        starPosY = (x1,j)
+        endPosY  = (x2,j)
+        cuadro.append([i,j,i+distanciaX,j+distanciaY])
+        width = 1
+        pygame.draw.line(screen, color, starPosX, endPosX, width)
+        pygame.draw.line(screen, color, starPosY, endPosY, width)
+
+        i  = i + distanciaX
+        j = j + distanciaY
+
+def rellenaFigura(cuadro,screen):
+    colorB = 0
+    colorA = 0
+    black = (0,0,0,0)
+    color = (0,0,0)
+    for  c in cuadro:
+        i = c[1]
+        while i <  c[2]:
+            j = c[0]
+            while j < c[3]:
+                actual = screen.get_at((int(c[0]+1),int(c[1]+1)))
+                # print("valor de actual",  actual)
+                if actual != black : 
+                    colorB = colorB + 1
+                    color = actual
+                else:
+                    colorA = colorA + 1
+                j = j + 1
+            i = i + 1
+        y = c[1]
+        if(colorB > colorA):
+            while y < c[3]:
+                x = c[0]
+                while x < c[2]:
+                    screen.set_at((x,y),color) 
+                    x = x + 1
+                y = y + 1
+        
+    print("fin")
+#####################################################################################################
+
+
 # Inicializamos pygame
 pygame.init()
 
@@ -23,15 +82,18 @@ screen.blit(imagen,(100,100))
 TextButton= pygame.draw.rect(screen,(12,100,0), (50, 10, 200,20),2)
 
 x1 = 100
-y1 = 10
+y1 = 100
 x2 = x1 + tamImagen.get_width()
 y2 = y1 + tamImagen.get_height()
 
 print("El ancho es ", x2)
 print("El alto es", y2)
 
+cuadro = [] #lista
 
-n = int(input("Tam de la matriz: "))
+r = obtenTam()
+generaCuadricula(r,x1,y1,x2,y2,screen, cuadro)
+rellenaFigura(cuadro,screen)
 pygame.display.update()
 
 # Comenzamos el bucle del juego
